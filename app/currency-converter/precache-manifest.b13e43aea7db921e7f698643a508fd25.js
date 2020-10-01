41cec5f62656096f82f8352967f792d835e8a072
Udjin Stolboviy <UdjinStolbovy@gmail.com>
2020-10-01 20:08:51 +0300

    currency-converter

---
+++ app/currency-converter/precache-manifest.b13e43aea7db921e7f698643a508fd25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d004c43f9a5ea0fb1baef9410ba50504",
+    "url": "/ua/app/currency-converter/index.html"
+  },
+  {
+    "revision": "a83c47bb27505f193189",
+    "url": "/ua/app/currency-converter/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5d096068eca048073c05",
+    "url": "/ua/app/currency-converter/static/js/2.34db69e8.chunk.js"
+  },
+  {
+    "revision": "882fee5ad56cfd78b1cc5d94b617aa42",
+    "url": "/ua/app/currency-converter/static/js/2.34db69e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a83c47bb27505f193189",
+    "url": "/ua/app/currency-converter/static/js/main.f59a572b.chunk.js"
+  },
+  {
+    "revision": "f1e192e1991ad3d804b2",
+    "url": "/ua/app/currency-converter/static/js/runtime-main.fdeb06f0.js"
+  }
+]);
