8f8174aed416333fce1e63762a71183ac729e5f0
Udjin Stolboviy <JenyJenyy@gmail.com>
2021-06-15 16:32:47 +0300

    new project

--- js/script.js
+++
@@ -1,9 +0,0 @@
-/* Установите ширину боковой навигации до 250 пикселей */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-
-/* Установите ширину боковой навигации на 0 */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
