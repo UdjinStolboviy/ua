d8c1cd53e0d162642c22da6b7ce288dcd7cc5761
Yvgen Stolbovyii <stolbovyii@mydigicode.com>
2023-03-30 19:15:43 +0200

    add files

--- components/ButtonGroup/styleButtonGroup.ts
+++
@@ -1,8 +0,0 @@
-import styled from "@emotion/styled";
-
-
-export const ButtonGroupStyle = styled.div`
-    margin-top: 14px;
-    margin-left: 30px;
-  
-`;
--- components/ImageLoader.ts
+++
@@ -1,5 +0,0 @@
-import { ImageLoader, ImageLoaderProps } from "next/image";
-
-export const imageLoader: ImageLoader = (resolverProps: ImageLoaderProps) => {
-    return resolverProps.src;
-};
